32fd8feab84778f55544004226f773420caf4110
ericmahare <erikmahare@gmail.com>
2022-04-05 00:49:50 +0300

    fix linter errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,24 @@
-//get dom elements
-const closeBtn = document.querySelector('.close-container')
-const hambager = document.querySelector('.nav_bars')
-const mobNav = document.querySelector('.mobile_menu')
-const mobLinks = document.querySelectorAll('.mobile-link')
+/* eslint-disable linebreak-style */
+const closeBtn = document.querySelector('.close-container');
+const hambager = document.querySelector('.nav_bars');
+const mobNav = document.querySelector('.mobile_menu');
+const mobLinks = document.querySelectorAll('.mobile-link');
 
-//hide the mobile menu
 closeBtn.addEventListener('click', () => {
   mobNav.style.cssText = `
   position: fixed;
   top: -9999px;
   left: -9999px;
-`
-})
+`;
+});
 
-//show the mobile menu
 hambager.addEventListener('click', () => {
   mobNav.style.cssText = `
   position: fixed;
   top: 0;
   left: 0;
-  `
-})
+  `;
+});
 
 mobLinks.forEach((link) => {
   link.addEventListener('click', () => {
@@ -28,6 +26,6 @@ mobLinks.forEach((link) => {
     position: fixed;
     top: -999px;
     left: -999px;
-  `
-  })
-})
+  `;
+  });
+});
